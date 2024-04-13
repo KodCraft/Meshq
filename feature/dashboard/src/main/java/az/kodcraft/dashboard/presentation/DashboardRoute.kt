cff3bbd27923623e0340ddfd03d5167d6cd790c2
firuza.aliyeva <firuza.aliyeva@azintelecom.az>
2024-04-13 11:03:39 +0400

    feat 7 month dropdown added to the dashboard

--- feature/dashboard/src/main/java/az/kodcraft/dashboard/presentation/DashboardRoute.kt
+++ feature/dashboard/src/main/java/az/kodcraft/dashboard/presentation/DashboardRoute.kt
@@ -19,13 +19,20 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -38,6 +45,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import az.kodcraft.core.presentation.composable.appBar.TopAppBar
 import az.kodcraft.core.presentation.theme.PrimaryTurq
 import az.kodcraft.core.presentation.theme.body
+import az.kodcraft.core.presentation.theme.bodyLight
 import az.kodcraft.core.presentation.theme.footNote
 import az.kodcraft.core.presentation.theme.largeTitle
 import az.kodcraft.core.presentation.theme.smallTitle
@@ -48,6 +56,9 @@ import az.kodcraft.dashboard.presentation.composables.WeekRow
 import az.kodcraft.dashboard.presentation.contract.DashboardIntent
 import az.kodcraft.dashboard.presentation.contract.DashboardUiState
 import az.kodcraft.dashboard.presentation.utils.getWeekData
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.Locale
 
 
 @Composable
@@ -87,12 +98,42 @@ fun DashboardScreen(
                 color = Color.White,
                 style = MaterialTheme.typography.largeTitle
             )
-            Spacer(modifier = Modifier.height(30.dp))
-
+            Spacer(modifier = Modifier.height(20.dp))
 
             val listState = rememberLazyListState(
                 initialFirstVisibleItemIndex = uiState.startIndex,
             )
+            // Observe the first visible item index
+            val visibleWeekIndex by remember {
+                derivedStateOf {
+                    listState.firstVisibleItemIndex
+                }
+            }
+
+            MonthDropdown(
+                modifier = Modifier.align(Alignment.End),
+                selectedMonth = uiState.selectedDay.month.getDisplayName(
+                    TextStyle.FULL,
+                    Locale.getDefault()
+                ),
+                onMonthSelected = {onIntent.invoke(DashboardIntent.SetDay(it, visibleWeekIndex))},
+                year = uiState.selectedDay.year
+            )
+
+            // Update selected day when week changes
+            LaunchedEffect(visibleWeekIndex) {
+                val weekData = uiState.selectedWeek(visibleWeekIndex).getWeekData()
+                val dayIndex = uiState.selectedDay.dayOfWeek.value - 1
+                if (dayIndex != -1) {
+                    onIntent.invoke(
+                        DashboardIntent.SetDay(
+                            weekData.weekDays[dayIndex].day,
+                            visibleWeekIndex
+                        )
+                    )
+                }
+            }
+
 
             LazyRow(
                 modifier = Modifier
@@ -142,12 +183,70 @@ fun DashboardScreen(
 const val PageCount = 20_000
 const val StartIndex = PageCount / 2
 
+
+@Composable
+fun MonthDropdown(
+    selectedMonth: String, // This could be adjusted to use LocalDate or another appropriate type
+    onMonthSelected: (LocalDate) -> Unit,
+    modifier: Modifier = Modifier,
+    year: Int  // Default to current year, adjustable as needed
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var displayMonth by remember(selectedMonth) { mutableStateOf(selectedMonth) }
+
+    // Generate month names using LocalDate and the default locale
+    val months = (1..12).map {
+        LocalDate.of(2022, it, 1).month.getDisplayName(TextStyle.FULL, Locale.getDefault())
+    }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = displayMonth,
+                style = MaterialTheme.typography.bodyLight,
+                modifier = Modifier
+                    .noRippleClickable { expanded = true }
+                    .padding(8.dp)
+            )
+            Icon(
+                painter = painterResource(id = com.google.android.material.R.drawable.mtrl_ic_arrow_drop_down),
+                contentDescription = "month dropdown",
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+        // Dropdown menu that shows when expanded is true
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .height(160.dp)  // Adjust height to fit the UI design
+                .padding(top = 4.dp)
+        ) {
+            months.forEachIndexed { index, month ->
+                DropdownMenuItem(
+                    text = { Text(month) },
+                    onClick = {
+                        displayMonth = month
+                        // Calculate the first day of the selected month
+                        val firstDayOfMonth = LocalDate.of(year, index + 1, 1)
+                        onMonthSelected(firstDayOfMonth)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+
 @Composable
 fun Workouts(workouts: List<DashboardWeekWorkoutDm>, onWorkoutClick: (id: String) -> Unit) {
     LazyColumn(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         items(workouts) {
             Card(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(185.dp)
                     .noRippleClickable { onWorkoutClick(it.id) },
                 colors = if (it.isSelected) CardDefaults.cardColors()
@@ -169,7 +268,11 @@ fun Workouts(workouts: List<DashboardWeekWorkoutDm>, onWorkoutClick: (id: String
                             else MaterialTheme.typography.body
                         )
                     }
-                    WorkoutDate(it.date, modifier = Modifier.padding(top = 18.dp, end = 25.dp).align(Alignment.TopEnd))
+                    WorkoutDate(
+                        it.date, modifier = Modifier
+                            .padding(top = 18.dp, end = 25.dp)
+                            .align(Alignment.TopEnd)
+                    )
 
                     Box(
                         modifier = Modifier
@@ -178,7 +281,9 @@ fun Workouts(workouts: List<DashboardWeekWorkoutDm>, onWorkoutClick: (id: String
                                 Brush.verticalGradient(
                                     colors = listOf(
                                         Color.Transparent,
-                                       if(it.isSelected) PrimaryTurq.copy(0.95f) else  Color.White.copy(alpha = 0.95f)
+                                        if (it.isSelected) PrimaryTurq.copy(0.95f) else Color.White.copy(
+                                            alpha = 0.95f
+                                        )
                                     ),
                                     startY = 300f, // You might need to adjust this value
                                     endY = Float.POSITIVE_INFINITY
@@ -193,7 +298,7 @@ fun Workouts(workouts: List<DashboardWeekWorkoutDm>, onWorkoutClick: (id: String
 }
 
 @Composable
-fun WorkoutDate(date: String, modifier:Modifier) {
+fun WorkoutDate(date: String, modifier: Modifier) {
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         Icon(
             painter = painterResource(id = az.kodcraft.core.R.drawable.ic_calendar),
