3eb2adc11bfe5935f7a289302ec41d1ef1d10c0e
firuza.aliyeva <firuza.aliyeva@azintelecom.az>
2024-04-13 14:47:06 +0400

    auth

---
+++ feature/auth/src/main/java/az/kodcraft/auth/navigation/AuthGraph.kt
@@ -0,0 +1,13 @@
+package az.kodcraft.auth.navigation
+
+//import androidx.navigation.NavGraphBuilder
+//import androidx.navigation.compose.composable
+//import az.kodcraft.dashboard.presentation.DashboardRoute
+
+//fun NavGraphBuilder.authGraph(
+//    navigateToWorkoutDetails:(id: String)-> Unit
+//) {
+////    composable(route = AuthRouteConstants.LOGIN_SCREEN) {
+////        DashboardRoute(navigateToWorkoutDetails = navigateToWorkoutDetails)
+////    }
+//}
